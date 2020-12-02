4b8dd55e1dff539734d4f9c6090085a984dd1d87
Tetsuya Yasuki <ktm.htm.rtm.405@hotmail.co.jp>
2020-12-02 21:49:52 +0900

    practice jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div><h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
 }
+
+// class App extends Component {
+//   render(){
+//     return React.createElement(
+//       "div",null,"hello world!!"
+//     );
+//   }
+// }
 
 export default App;
